c384d49bc48fdddfbc0289dd3299b80375d2e5a1
Jussi Mäkelä <jussi.makela@solinet.fi>
2025-10-02 13:47:22 +0300

    Complete task A4_T1

---
+++ week4/A4_T1.py
@@ -0,0 +1,13 @@
+# Course task: A4_T1 For-loop 1
+
+print("Program starting.")
+
+value1 = int(input("Insert starting value: "))
+value2 = int(input("Insert stopping value: "))
+
+print("Starting for-loop.")
+
+for i in range(value1, value2 + 1):
+    print(i)
+
+print("Program ending.")
