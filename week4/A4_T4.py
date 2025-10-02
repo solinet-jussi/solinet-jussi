00f6503f1855086e64d3e4041f52927629f5c48b
Jussi Mäkelä <jussi.makela@solinet.fi>
2025-10-02 14:07:56 +0300

    Complete task A4_T4

---
+++ week4/A4_T4.py
@@ -0,0 +1,23 @@
+# Course task: A4_T4 Repetitive prompt
+
+print("Program starting.")
+
+words = []
+word = None
+
+while word != "":
+    word = input("Enter a word (empty string stops the program): ")
+    if word == "":
+        break
+    words.append(word)
+
+word_count = len(words)
+char_count = 0
+for word in words:
+    char_count += len(word)
+
+print("You entered:")
+print(f"– {word_count} words")
+print(f"– {char_count} characters")
+
+print("Program ending.")
